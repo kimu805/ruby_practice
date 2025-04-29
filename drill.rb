4a94260b85dff4bb8bf5d1e81acdf54f67643033
kimu805 <kimukimuok805@gmail.com>
2025-04-29 18:53:22 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,5 +1,19 @@
-begin
-  raise "Err!"
-rescue => e
-  puts e.class
+m = Module.new
+
+CONST = "Constant in Toplevel"
+
+_proc = Proc.new do
+  CONST = "Constant in Proc"
 end
+
+m.instance_eval(<<-EOS)
+  CONST = "Constant in Module instance"
+
+  def const
+    CONST
+  end
+EOS
+
+m.module_eval(&_proc)
+
+p m.const
