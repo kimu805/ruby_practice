befca14131d38763ab3314416d1e404b8409091f
kimu805 <kimukimuok805@gmail.com>
2025-05-02 14:26:40 +0900

    succメソッドの確認

--- drill.rb
+++ drill.rb
@@ -1,30 +1,4 @@
-class C
-end
+array = ["a", "b", "c"]
+array = array.map!{|content| content.succ}
 
-module M
-  refine C do
-    def m1(value)
-      p "define m1 using Refinement"
-      super value - 100 # 300 - 100
-    end
-  end
-end
-
-class C
-  def m1(value)
-    p "define m1 in C"
-    value - 100 # 200 - 100
-  end
-end
-
-using M # ここからRefinementが有効になる
-
-class K < C
-  def m1(value)
-    p "define m1 in K"
-    super value - 100 # 400 - 100
-    # Refinementが有効なのでsuperはモジュールMにあるm1を参照する
-  end
-end
-
-puts K.new.m1 400
+p array
