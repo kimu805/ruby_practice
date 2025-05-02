28ca266c8d6f52ac8f935895463bb64ea9fe9c54
kimu805 <kimukimuok805@gmail.com>
2025-05-02 10:40:18 +0900

    refinement

--- drill.rb
+++ drill.rb
@@ -1,11 +1,30 @@
+class C
+end
+
 module M
-  def class_m
-    "class_m"
+  refine C do
+    def m1(value)
+      p "define m1 using Refinement"
+      super value - 100 # 300 - 100
+    end
   end
 end
 
 class C
-  extend M
+  def m1(value)
+    p "define m1 in C"
+    value - 100 # 200 - 100
+  end
 end
 
-p C.new.methods.include? :class_m
+using M # ここからRefinementが有効になる
+
+class K < C
+  def m1(value)
+    p "define m1 in K"
+    super value - 100 # 400 - 100
+    # Refinementが有効なのでsuperはモジュールMにあるm1を参照する
+  end
+end
+
+puts K.new.m1 400
