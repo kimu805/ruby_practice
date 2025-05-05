bc069a6a134450303508a428e0e60e9b65528c4f
kimu805 <kimukimuok805@gmail.com>
2025-05-05 18:23:21 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,2 +1,24 @@
-idols = ['TWICE', 'IZ*ONE', 'aespa', 'BLACKPINK', 'aespa', 'TWICE']
-p idols.tally
+class A
+  def foo
+    puts "A"
+  end
+
+  def bar
+    p "淘汰。"
+    foo
+  end
+end
+
+module M
+  refine A do
+    def foo
+      puts "B"
+    end
+  end
+end
+
+x = A.new
+x.foo # => A
+using M
+x.foo # => B
+x.bar # => _(1)_ # ここを問う問題
