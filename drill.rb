499b7b87fd4c65fac158aebf302d0226318a2942
kimu805 <kimukimuok805@gmail.com>
2025-05-07 11:15:37 +0900

    selfの確認

--- drill.rb
+++ drill.rb
@@ -1,24 +1,10 @@
-class A
-  def foo
-    puts "A"
+class Hoge
+  def fuga(o = nil)
+    hoge
   end
-
-  def bar
-    p "淘汰。"
-    foo
-  end
-end
-
-module M
-  refine A do
-    def foo
-      puts "B"
-    end
+  private
+  def hoge 
+    puts "Hoge"
   end
 end
-
-x = A.new
-x.foo # => A
-using M
-x.foo # => B
-x.bar # => _(1)_ # ここを問う問題
+Hoge.new.fuga(Hoge.new)
