a8565a40db0d2f4b8889f6e365d3d521cc34bd4e
kimu805 <kimukimuok805@gmail.com>
2025-05-08 12:51:00 +0900

    CONSTの検証

--- drill.rb
+++ drill.rb
@@ -1,13 +1,22 @@
-class Speaker
-  @message = "Hello!"
-
-  class << self
-    @message = "Howdy!"
-
-    def speak
-      @message
-    end
+module M
+  def refer_const
+    CONST
   end
 end
 
-puts Speaker.singleton_class.instance_variable_get(:@message)
+module E
+  CONST = '010'
+end
+
+class D
+  CONST = "001"
+end
+
+class C < D
+  CONST = '100'
+  include E
+  include M
+end
+
+c = C.new
+p c.refer_const
