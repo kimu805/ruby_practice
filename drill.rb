5acdeb339f3de3ef1e31adac0a53a698517b5153
kimu805 <kimukimuok805@gmail.com>
2025-05-14 16:53:44 +0900

    Update drill.rb

--- drill.rb
+++ drill.rb
@@ -1,1 +1,17 @@
-p "kimura".capitalize
+class Class
+  def method_missing(id,*args)
+    puts "Class#method_missing"
+  end
+end
+class A 
+  def method_missing(id,*args)
+    puts "A#method_missing"
+  end
+end
+class B < A
+  def method_missing(id,*args)
+    puts "B#method_missing"
+  end
+end
+
+B.dummy_method
