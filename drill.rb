84ae67094071748b3c3673940fa4171940209187
kimu805 <kimukimuok805@gmail.com>
2025-05-15 12:57:56 +0900

    inherited

--- drill.rb
+++ drill.rb
@@ -1,18 +1,10 @@
-class SecretKeeper
-  def initialize(secret)
-    @secret = secret
+class String
+  def self.inherited(subclass)
+    puts "#{self}は#{subclass}に継承された"
   end
 end
 
-obj = SecretKeeper.new(42)
+class MyString < String
 
-# instance_evalを使ってプライベート変数にアクセス
-obj.instance_eval do
-  puts "秘密の値: #{@secret}"
-
-  def reveal
-    "秘密は#{@secret}です"
-  end
 end
 
-puts obj.reveal
