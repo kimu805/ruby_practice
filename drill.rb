89a604f5538619c4338764f6ee23507dbae9824c
kimu805 <kimukimuok805@gmail.com>
2026-01-24 12:54:38 +0900

    commit

--- drill.rb
+++ drill.rb
@@ -1,6 +1,11 @@
 array = [64, 25, 12, 42, 11]
 
-
+def bubble_sort(array)
+  size = array.size
+  (0...size - 1).each do |i|
+    
+  end
+end
 
 bubble_sort(array)
 puts "bubble sorted array: #{array}"
