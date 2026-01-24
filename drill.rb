f78b1a03af710f9d97db15e983c68686edb6abdd
kimu805 <kimukimuok805@gmail.com>
2026-01-24 13:14:24 +0900

    commit

--- drill.rb
+++ drill.rb
@@ -2,8 +2,10 @@ array = [64, 25, 12, 42, 11]
 
 def bubble_sort(array)
   size = array.size
-  (0...size - 1).each do |i|
-    
+  (0...size).each do |i|
+    if array[i] > array[i + 1]
+      array[i], array[i + 1] = array[i + 1], array[i]
+    end
   end
 end
 
